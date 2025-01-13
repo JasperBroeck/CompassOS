608b2246816c8013c6164ae2b22733722f441c76
Jasper Van den Broeck <jaspervandenbroeck2@proton.me>
2025-01-13 15:30:08 +0100

    finishing

--- install.sh
+++ install.sh
@@ -35,12 +35,6 @@ sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
 cd ..
 sudo cp -r pacman.conf /etc/pacman.conf
 
@@ -53,7 +47,7 @@ echo " ### Bloating your system completely... ### "
 echo " ########################################## "
 echo ""
 sleep 0.5
-paru -S  freecad musescore btop steam kdenlive visual-studio-code-bin tahoma2d gdm gnome-text-editor gnome-disk-utility gdm hyprland hyprpaper waybar wofi firefox nautilus nano git wget curl dpkg discord minecraft-launcher balena-etcher github-desktop spotify obsidian kitty hyprshot
+paru -S  freecad musescore btop steam kdenlive visual-studio-code-bin gdm gnome-text-editor gnome-disk-utility gdm hyprland hyprpaper waybar wofi firefox nautilus nano git wget curl dpkg discord minecraft-launcher balena-etcher github-desktop spotify obsidian kitty hyprshot blueman ttf-font-awesome
 
 clear
 
@@ -63,9 +57,10 @@ echo " ################################# "
 echo ""
 sleep 0.5
 cd config
-sudo cp -r hypr/ ~/.config/hypr/
-sudo cp -r waybar/ ~/.config/waybar/
-sudo cp -r wofi/ ~/.config/wofi/
+sudo cp -r hypr/ ~/.config/
+sudo cp -r waybar/ ~/.config/
+sudo cp -r wofi/ ~/.config/
+cd ..
 sudo cp -r wallpaper.jpg ~/wallpaper.jpg
 
 clear
@@ -76,7 +71,9 @@ echo " ###################################### "
 echo ""
 sleep 0.5
 sudo systemctl enable gdm
-sudo systemctl start gdm --now
+sudo systemctl start gdm
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
 
 clear
 
